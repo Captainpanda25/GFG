13c6e94913fe1d31ebc18f0257e4b54982e12fd8
Arya Dev <35726067+Captainpanda25@users.noreply.github.com>
2020-09-10 23:36:45 +0530

    Add files via upload
    c - 01

---
+++ rta.java
@@ -0,0 +1,51 @@
+//To Rotate 2D array 90 degrees clockwise 
+import java.util.*;
+public class rta {
+
+	public static void main(String[] args) {
+		int T,N,i,j,k,c,m;
+		Scanner in = new Scanner(System.in);
+		T=in.nextInt();
+	    for(i=0;i<T;i++)
+	    {
+	    	N=in.nextInt();
+	    	int arr[][]=new int[N][N];
+	    	for(j=0;j<N;j++)
+	    	{
+	    		for(k=0;k<N;k++)
+	    		{
+	    			arr[j][k]=in.nextInt();
+	    		}
+	    	}
+	    	for(j=0;j<N;j++)
+	    	{
+	    		for(k=0;k<j;k++)
+	    		{
+	    			c=arr[j][k];
+	    			arr[j][k]=arr[k][j];
+	    			arr[k][j]=c;
+	    		}
+	    	}
+	    	for(j=0;j<N;j++)
+	    	{
+	    		for(k=N-1,m=0;k>m;k--,m++)
+	    		{
+	    			c=arr[j][m];
+	    			arr[j][m]=arr[j][k];
+	    			arr[j][k]=c;
+	    		}
+	    	}
+	    	
+	    	for(j=0;j<N;j++)
+	    	{
+	    		for(k=0;k<N;k++)
+	    		{
+	    			System.out.println(arr[j][k]+" ");;
+	    		}
+	    	}
+	    	
+	    }
+
+	}
+
+}
